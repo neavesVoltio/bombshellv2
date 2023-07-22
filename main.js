f99079098a6c4e783c92ebdc18c1a3a21d48e5d3
Neaves Voltio <neaves@voltio.us>
2023-07-22 14:19:21 -0600

    update domain

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ import { loginCheck } from './app/loginCheck.js'
 
 import './app/logout.js'
 let currentDomain = window.location.origin
-export let webDomain = //'https://voltioenergy.com/bombshell'
- 'http://localhost:5500'
+export let webDomain = 
+// 'http://localhost:5500'
 // 'https://neavesvoltio.github.io'
 // 'https://voltioenergy.com/bombshell'
+ 'https://neavesvoltio.github.io/bombshellv2'
   
 onAuthStateChanged(auth, async (user) => {
     try{
