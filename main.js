b5ccaf9aa7f9af443923b639fcf30bc421a8ef07
Neaves Voltio <neaves@voltio.us>
2023-07-22 14:15:22 -0600

    upload

---
+++ main.js
@@ -0,0 +1,31 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+import { auth } from './app/firebase.js'
+import { loginCheck } from './app/loginCheck.js'
+
+
+import './app/logout.js'
+let currentDomain = window.location.origin
+export let webDomain = //'https://voltioenergy.com/bombshell'
+ 'http://localhost:5500'
+// 'https://neavesvoltio.github.io'
+// 'https://voltioenergy.com/bombshell'
+  
+onAuthStateChanged(auth, async (user) => {
+    try{
+    
+    if(user){
+        loginCheck(user)
+    } else {
+        loginCheck(user)
+    }
+    }catch(error){
+        console.log(error)
+    }
+    
+})
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
